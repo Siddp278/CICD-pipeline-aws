3d495f599f3d778c1aee5dd2f8113de5c43fa812
Siddharth Patra <siddp278@gmail.com>
2023-03-09 08:50:53 -0600

    changes in yml file

--- django_github_actions_aws/wsgi.py
+++ django_github_actions_aws/wsgi.py
@@ -7,6 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
+"""
+By default, Elastic Beanstalk looks for a file named application.py in our project. It uses that file to run
+ our application, but we don't have that file in our project. Do we? We need to tell Elastic Beanstalk to 
+ use the wsgi.py file in our project to run our application instead.
+
+Create a folder named .ebextensions in your project directory. In that folder create a config file. 
+"""
+
 import os
 
 from django.core.wsgi import get_wsgi_application
